406361772ee0456c83de82bc8fd9dfb4fb40c21c
Cantepie <adrien.cantepie@6tm.com>
2021-05-23 01:12:55 +0200

    remove unused (ans specific) utils method

--- Bundle/CoreBundle/src/Utils/Utils.php
+++ Bundle/CoreBundle/src/Utils/Utils.php
@@ -7,23 +7,12 @@ namespace Umbrella\CoreBundle\Utils;
  */
 class Utils
 {
-    const ALPHANUM = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    const ALPHA_UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
     public static function type_class_to_id(string $typeClass): string
     {
         $ns = preg_replace('/Type$/', '', $typeClass);
         $name = str_replace('\\', '_', $ns);
 
         return \function_exists('mb_strtolower') && preg_match('//u', $name) ? mb_strtolower($name, 'UTF-8') : strtolower($name);
-    }
-
-    /**
-     * @see https://stackoverflow.com/questions/6167279/converting-a-simplexml-object-to-an-array?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-     */
-    public static function xml_2_array($xmlObject): array
-    {
-        return json_decode(json_encode((array) $xmlObject), true);
     }
 
     /**
@@ -44,34 +33,5 @@ class Utils
         $bytes /= pow(1024, $pow);
 
         return round($bytes, $precision) . ' ' . $units[$pow];
-    }
-
-    /**
-     * @see http://stackoverflow.com/questions/1993721/how-to-convert-camelcase-to-camel-case
-     *
-     * @param string $input
-     */
-    public static function to_underscore($input): string
-    {
-        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
-        $ret = $matches[0];
-        foreach ($ret as &$match) {
-            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
-        }
-
-        return implode('_', $ret);
-    }
-
-    public static function random(int $length = 8, string $characters = self::ALPHANUM): string
-    {
-        $str = '';
-
-        $max = strlen($characters) - 1;
-        for ($i = 0; $i < $length; ++$i) {
-            $rand = mt_rand(0, $max);
-            $str .= $characters[$rand];
-        }
-
-        return str_shuffle($str);
     }
 }
