3bbff99c517de3df8a51edd848041d6a7ea26f75
Cantepie <adrien.cantepie@6tm.com>
2021-06-04 20:09:40 +0200

    [fix] Use security sf form_login authenticator + fix Exception message for password checker

--- Bundle/AdminBundle/src/Security/UserChecker.php
+++ Bundle/AdminBundle/src/Security/UserChecker.php
@@ -2,32 +2,17 @@
 
 namespace Umbrella\AdminBundle\Security;
 
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Exception\AccountStatusException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Umbrella\AdminBundle\Model\AdminUserInterface;
-use Umbrella\AdminBundle\Security\Exception\AccountDisabledException;
 
 /**
  * Class UserChecker
  */
 class UserChecker implements UserCheckerInterface
 {
-    private RouterInterface $router;
-
-    private ParameterBagInterface $parameters;
-
-    /**
-     * UserChecker constructor.
-     */
-    public function __construct(RouterInterface $router, ParameterBagInterface $parameters)
-    {
-        $this->router = $router;
-        $this->parameters = $parameters;
-    }
-
     /**
      * Checks the user account before authentication.
      *
@@ -40,10 +25,7 @@ class UserChecker implements UserCheckerInterface
         }
 
         if (!$user->isActive()) {
-            $e = new AccountDisabledException();
-            $e->setUser($user);
-
-            throw $e;
+            throw new CustomUserMessageAccountStatusException('account_disabled');
         }
     }
 
