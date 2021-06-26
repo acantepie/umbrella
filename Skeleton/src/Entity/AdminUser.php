6a1b87a743d2fc3187367694ffeeedb7b69b3a01
Cantepie <adrien.cantepie@6tm.com>
2021-06-27 01:32:50 +0200

    [User / User profile] can now be totally disabled

--- Skeleton/src/Entity/AdminUser.php
+++ Skeleton/src/Entity/AdminUser.php
@@ -4,7 +4,7 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Umbrella\AdminBundle\Entity\BaseUser;
+use Umbrella\AdminBundle\Entity\BaseAdminUser;
 use Umbrella\CoreBundle\Search\Annotation\Searchable;
 
 /**
@@ -15,7 +15,7 @@ use Umbrella\CoreBundle\Search\Annotation\Searchable;
  * @Searchable
  * @UniqueEntity("email")
  */
-class User extends BaseUser
+class AdminUser extends BaseAdminUser
 {
     /**
      * {@inheritDoc}
