c5770cdde5d51b02f83610e093bdd876ec2d972d
Cantepie <adrien.cantepie@6tm.com>
2021-11-27 23:21:43 +0100

    Ui

--- Bundle/CoreBundle/assets/components/PasswordTogglable.js
+++ Bundle/CoreBundle/assets/components/PasswordTogglable.js
@@ -3,24 +3,25 @@ export default class PasswordTogglable extends HTMLDivElement {
     constructor() {
         super()
         this.showPassword = false
-        this.classList.add('input-group')
+        this.classList.add('input-icon')
 
         this.onClick = this.onClick.bind(this)
     }
 
     connectedCallback() {
-        const e = document.createElement('div')
-        e.className = 'input-group-text'
+        const e = document.createElement('a')
+        e.href = '#'
+        e.className = 'input-icon-addon'
         e.innerHTML = `${this.icon()}`
 
         this.appendChild(e)
-        this.querySelector('.input-group-text').addEventListener('click', this.onClick)
+        this.querySelector('.input-icon-addon').addEventListener('click', this.onClick)
     }
 
     onClick(e) {
         e.preventDefault()
         this.showPassword = !this.showPassword
-        this.querySelector('.input-group-text').innerHTML = this.icon()
+        this.querySelector('.input-icon-addon').innerHTML = this.icon()
         this.querySelector('input').type = this.showPassword ? 'text' : 'password'
     }
 
